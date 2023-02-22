8221f2bc25b2aed1f03fd02b4ee255453c8896e1
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-02-22 16:24:05 +0700

    Revisi Deploy II

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const multer = require("multer");
+const cors = require("cors");
 
 require("dotenv").config();
 const indexRouter = require("./routes/index");
@@ -21,6 +22,7 @@ const refreshTokenRouter = require("./routes/refreshTokens");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
